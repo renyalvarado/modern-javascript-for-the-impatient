0b7412924281cff5ecb4e1d1ef2a124fb300181b
Reny Alvarado <renyalvarado@gmail.com>
2023-01-02 23:52:25 -0300

    Add answers 3-5 for Chapter 06

--- ch06/answers.js
+++ ch06/answers.js
@@ -27,7 +27,7 @@ console.log(
     `Original String: ${exercise01Str}, `,
     `Escaped String ${toEscapedString(exercise01Str)}`
 );
-
+console.log("\n")
 
 // # 2
 // https://stackoverflow.com/questions/69032512/can-the-the-string-with-2-different-codepoints-which-form-1-character-consider
@@ -57,3 +57,62 @@ const exercise02Str = "This character (💛) is a yellow heart";
 for (let i = 0; i <= exercise02Str.length + 3; i++) {
     console.log(i, fitsString(exercise02Str, i));
 }
+console.log("\n");
+
+
+// # 3
+const weirdStrinFunctionParams = [
+  NaN,
+  -NaN,
+  Infinity,
+  -Infinity,
+  0,
+  1,
+  2,
+  "string",
+  [],
+  {},
+  null,
+  undefined,
+];
+const exercise03Str = "This is a big string";
+for(const param01 of weirdStrinFunctionParams) {
+    for(const param02 of weirdStrinFunctionParams) {
+        console.log(
+            `exercise03Str.slice(${param01}, ${param02}): `, 
+            exercise03Str.slice(param01, param02)
+        );
+    }
+}
+console.log("\n");
+
+
+// # 4
+function getAllSubstrings(str){
+    const substrings = new Set();
+    const chars = [...str];
+    for (let i = 0; i < chars.length; i++) {
+        for (let j = i + 1; j <= chars.length; j++) {
+            const subchars = chars.slice(i, j);
+            substrings.add(subchars.join(""));
+        }
+    }
+    return [...substrings];
+}
+const exercise04Str = "This 💛 is yellow";
+console.log(
+    exercise04Str,
+    getAllSubstrings(exercise04Str)
+);
+console.log("\n");
+
+
+// # 5
+function otherSlice(str, start, end) {
+    return [...str].slice(start, end).join("");
+}
+
+const exercise05Str = "This char💛is a yellow heart";
+console.log(
+    otherSlice(exercise05Str, 8, 11)
+);
